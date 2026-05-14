3b1d2ae54f858b74ee3f7fb27354a696a9aacfc0
alex123566545 <139175706+alex123566545@users.noreply.github.com>
2026-05-14 18:54:13 -0500

    Update etl_gold.py

--- etl/etl_gold.py
+++ etl/etl_gold.py
@@ -118,7 +118,7 @@ def train_model(conn, pipeline_name):
 
     # modelo
     model = RandomForestRegressor(
-        n_estimators=300,
+        n_estimators=120,
         max_depth=15,
         min_samples_split=5,
         min_samples_leaf=2,
